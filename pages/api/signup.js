c38b22f493ec45e661756a72802c634acb35f4d4
Luis Alvarez <luis@zeit.co>
2020-05-04 14:55:00 -0500

    Added signup

---
+++ pages/api/signup.js
@@ -0,0 +1,26 @@
+import { createCustomer } from '../../lib/api/bigcommerce/customers'
+import { setLoginCookie } from '../../lib/api/auth/cookies'
+import isAllowedMethod from '../../lib/api/is-allowed-method'
+
+const METHODS = ['POST']
+
+export default async function signup(req, res) {
+  if (!isAllowedMethod(req, res, METHODS)) return
+
+  const { email, firstName, lastName, password } = req.body
+
+  // Weak validations, for a production application make sure to be more strict here
+  if (!email || !firstName || !lastName || !password) {
+    return res.status(400).json({
+      errors: [{ message: 'Invalid signup' }],
+    })
+  }
+
+  const data = { email, firstName, lastName, password }
+  const result = await createCustomer(data)
+  const customer = result.data[0]
+
+  await setLoginCookie(res, { id: customer.id })
+
+  return res.status(200).json({ done: true })
+}
